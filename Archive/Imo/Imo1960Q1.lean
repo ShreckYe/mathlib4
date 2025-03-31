49d536ea5f4b79411782602c888c917d739d5bbf
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-03-31 12:06:57 +0800

    Add a comment of alternative tactics

--- Archive/Imo/Imo1960Q1.lean
+++ Archive/Imo/Imo1960Q1.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Lacker
 -/
 import Mathlib.Data.Nat.Digits
+--import Mathlib.Tactic.Bound
 
 /-!
 # IMO 1960 Q1
@@ -52,7 +53,7 @@ theorem lt_1000 {n : ℕ} (h1 : ProblemPredicate n) : n < 1000 := by
     rw [← h1.left]
     refine Nat.lt_base_pow_length_digits ?_
     simp
-  omega
+  omega -- `lianrith` and `bound` work here too
 
 /-
 We do an exhaustive search to show that all results are covered by `SolutionPredicate`.
