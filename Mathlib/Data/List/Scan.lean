9d5861ab6dfe022653ba91dcc58495c7698bad27
Yongshun Ye <ShreckYe@gmail.com>
2025-08-27 17:59:55 +0800

    Fix the statement

--- Mathlib/Data/List/Scan.lean
+++ Mathlib/Data/List/Scan.lean
@@ -118,9 +118,8 @@ theorem get_scanl_eq_foldl_take {i : Fin (l.length + 1)} :
     (l.scanl f b).get (i.cast (l.length_scanl b).symm) = (l.take i).foldl f b :=
   getElem_scanl_eq_foldl_take' i.isLt
 
-theorem scanl_eq_foldl_f_cons :
-    -- Taking the `head` from `l` seems not viable as there is no way to prove that it's non-empty.
-    scanl f b l = foldl (fun l e => f (l.head sorry) e :: l) [] l := by
+theorem scanl_eq_foldl_f_cons [Inhabited β] :
+    scanl f b l = foldl (fun l e => f (l.head!) e :: l) [] l := by
   sorry
 
 /-! ### List.scanr -/
