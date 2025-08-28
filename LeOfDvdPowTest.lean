c921ed408517b046ddefc51df4a33d391608f28c
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 14:00:54 +0800

    WIP

--- LeOfDvdPowTest.lean
+++ LeOfDvdPowTest.lean
@@ -2,7 +2,7 @@ import Mathlib
 
 variable {α} [Dvd α] [HPow α ℕ α] [LT α] [One α]
 
-example {a : α} {b c : ℕ} (ha : 1 < a) (h : a ^ b ∣ a ^ c) : b ≤ c := by
+example {a : α} {b c : ℕ} (ha : 1 < a) /- This condition needs to be changed/generalized for `Polynomial`. -/ (h : a ^ b ∣ a ^ c) : b ≤ c := by
   hint
 
 #synth UniqueFactorizationMonoid (Polynomial ℤ)
