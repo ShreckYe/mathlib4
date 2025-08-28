5abce31e3fa1288953cba49ce2f12a32ca88a478
Yongshun Ye <ShreckYe@gmail.com>
2025-08-28 13:54:58 +0800

    WIP

--- LeOfDvdPowTest.lean
+++ LeOfDvdPowTest.lean
@@ -1,6 +1,6 @@
 import Mathlib
 
-variable {α} [Dvd α] [HPow α ℕ α]
+variable {α} [Dvd α] [HPow α ℕ α] [LT α] [One α]
 
-example {a : α} {b c : ℕ} : a ^ b ∣ a ^ c -> b ≤ c := by
+example {a : α} {b c : ℕ} (ha : 1 < a) (h : a ^ b ∣ a ^ c) : b ≤ c := by
   hint
