0ff9c589042c04562f5fd9a41b58254d4fffefbc
Yongshun Ye <ShreckYe@gmail.com>
2025-09-06 16:19:04 +0800

    Explore some existing definitions

---
+++ TupleMaxMin.lean
@@ -0,0 +1,17 @@
+import Mathlib
+
+#check Tuple.sort
+#check List.max?
+#check List.argmax
+#check Tuple.argmax
+#check Fin.argmax
+#check Tuple.fold
+#check Fin.foldl
+#check Fin.foldr
+
+#check Finset.sup'
+#check Finset.sup
+
+#check Finset.argmax
+#check Finset.maxOn
+#check Finset.maxOn'
