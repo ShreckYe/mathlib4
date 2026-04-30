2218a617a53e2a5cd3d6538ba26bc75035b6db45
Yongshun Ye <ShreckYe@gmail.com>
2026-04-30 16:12:31 +0800

    Move Test.lean into MonoidPowEqTest.lean

--- Test.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib
-
-example {α : Type*}
-    [CommMonoidWithZero α] [UniqueFactorizationMonoid α] [GCDMonoid α] {a b : α} {m n : ℕ} (hmn : GCDMonoid.gcd m n = 1) (h : a ^ m = b ^ n) :
-    ∃ c, a = c ^ n ∧ b = c ^ m := by
-  hint
-
-#check CancelCommMonoid
-#check exists_associated_pow_of_mul_eq_pow
